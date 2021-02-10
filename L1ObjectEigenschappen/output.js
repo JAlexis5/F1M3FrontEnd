d6e9eebbbee189414904cfe97cbe9ecba42a354a
Jelani Alexis <31781@ma-web.nl>
2021-02-10 22:54:27 +0100

    Les 1 stappen 5-11 afgemaakt

--- L1ObjectEigenschappen/output.js
+++ L1ObjectEigenschappen/output.js
@@ -1,10 +1,82 @@
 const uitvoer = document.getElementById('uitvoer');
 let dataObject;
 
+const dagWeekDisplay = (num) => {
+    switch (num) {
+        case 0: return 'zondag'; break
+        case 1: return 'maandag'; break
+        case 2: return 'dinsdag'; break
+        case 3: return 'woensdag'; break
+        case 4: return 'donderdag'; break
+        case 5: return 'vrijdag'; break
+        case 6: return 'zaterdag'; break
+        default: return num;
+    }
+}
+
+const maandDisplay = (num) => {
+    switch (num) {
+        case 0: return 'januari'; break
+        case 1: return 'februari'; break
+        case 2: return 'maart'; break
+        case 3: return 'april'; break
+        case 4: return 'mei'; break
+        case 5: return 'juni'; break
+        case 6: return 'juli'; break
+        case 7: return 'augustus'; break
+        case 8: return 'september'; break
+        case 9: return 'oktober'; break
+        case 10: return 'november'; break
+        case 11: return 'december'; break
+        default: return num;
+    }
+}
+
+const maakDatum = (num) => {
+    let datum = new Date(num);
+
+    let minuten = datum.getMinutes();
+    let uren = datum.getHours();
+    let dagWeek = datum.getDay();
+    let dagMaand = datum.getDate();
+    let maand = datum.getMonth();
+    let jaar = datum.getFullYear();
+    if (minuten < 10) {
+        minuten = '0' + minuten;
+    }
+
+    return `${dagWeekDisplay(dagWeek)} <br>
+    ${dagMaand} ${maandDisplay(maand)} ${jaar} ${uren}:${minuten}`
+}
+
 const uitvoeren = () => {
     // voert data uit in de div met de ID uitvoer
-    uitvoer.innerHTML = `<div>${dataObject[0].tijd} </div>`;
-    uitvoer.innerHTML += `<div>${dataObject[1].tempBuiten} </div>`;
+    let html = "";
+    dataObject.forEach( obj => {
+        html += `<div class="rij">`;
+        html += `<div>${maakDatum(obj.tijd)}</div>`;
+        html += `<div>${obj.tempBuiten} &deg;C</div>`;
+        html += `<div>${obj.tempBinnen} &deg;C</div>`;
+        html += `<div>${obj.tempGewenst} &deg;C</div>`;
+        if(obj.tempGewenst > obj.tempBinnen) {
+            html += `<div> <img class="icon" src="assets/vlam.svg" alt="CV aan"> </div>`;
+        } else {
+            html += `<div> <img class="icon" src="assets/vlamUit.svg" alt="CV uit"></div>`
+        }
+        if(obj.lichtKamer) {
+            html += `<div> <img class="icon" src="assets/lampAan.svg" alt="Lamp aan"> </div>`;
+        } else {
+            html += `<div> <img class="icon" src="assets/lampUit.svg" alt="Lamp uit"> </div>`;
+        }
+        if(obj.lichtBuiten) {
+            html += `<div> <img class="icon" src="assets/lampAan.svg" alt="Lamp aan"> </div>`;
+        } else {
+            html += `<div> <img class="icon" src="assets/lampUit.svg" alt="Lamp uit"> </div>`;
+        }
+        html += `</div>`;   
+    });
+
+    uitvoer.innerHTML = html;
 }
 
 const onstart = () => {
@@ -15,3 +87,4 @@ const onstart = () => {
 }
 
 document.addEventListener('DOMContentLoaded', onstart)
+
